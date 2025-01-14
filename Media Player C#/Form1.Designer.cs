def7f3fe8a37a710efa27e968da16ec1fc904eb3
Miranda Marques <137963450+OMirandinha@users.noreply.github.com>
2025-01-13 21:31:27 -0300

    Add files via upload

---
+++ Media Player C#/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace Media_Player_C_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.list = new System.Windows.Forms.ListBox();
+            this.fileName = new System.Windows.Forms.Label();
+            this.Duration = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.Player = new AxWMPLib.AxWindowsMediaPlayer();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Player)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1018, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.loadToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // loadToolStripMenuItem
+            // 
+            this.loadToolStripMenuItem.Name = "loadToolStripMenuItem";
+            this.loadToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
+            this.loadToolStripMenuItem.Text = "Load";
+            this.loadToolStripMenuItem.Click += new System.EventHandler(this.LoadFolderEvent);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.ShowAboutEvent);
+            // 
+            // list
+            // 
+            this.list.FormattingEnabled = true;
+            this.list.Location = new System.Drawing.Point(777, 27);
+            this.list.Name = "list";
+            this.list.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.list.Size = new System.Drawing.Size(229, 485);
+            this.list.TabIndex = 2;
+            this.list.SelectedIndexChanged += new System.EventHandler(this.PlaylistChanged);
+            // 
+            // fileName
+            // 
+            this.fileName.AutoSize = true;
+            this.fileName.Location = new System.Drawing.Point(0, 517);
+            this.fileName.Name = "fileName";
+            this.fileName.Size = new System.Drawing.Size(54, 13);
+            this.fileName.TabIndex = 3;
+            this.fileName.Text = "File Name";
+            // 
+            // Duration
+            // 
+            this.Duration.AutoSize = true;
+            this.Duration.Location = new System.Drawing.Point(774, 520);
+            this.Duration.Name = "Duration";
+            this.Duration.Size = new System.Drawing.Size(59, 13);
+            this.Duration.TabIndex = 4;
+            this.Duration.Text = "Duration: 0";
+            // 
+            // timer
+            // 
+            this.timer.Tick += new System.EventHandler(this.TimerEvent);
+            // 
+            // Player
+            // 
+            this.Player.Enabled = true;
+            this.Player.Location = new System.Drawing.Point(0, 27);
+            this.Player.Name = "Player";
+            this.Player.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("Player.OcxState")));
+            this.Player.Size = new System.Drawing.Size(771, 485);
+            this.Player.TabIndex = 1;
+            this.Player.PlayStateChange += new AxWMPLib._WMPOCXEvents_PlayStateChangeEventHandler(this.PlayerStateChangeEvent);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1018, 542);
+            this.Controls.Add(this.Duration);
+            this.Controls.Add(this.fileName);
+            this.Controls.Add(this.list);
+            this.Controls.Add(this.Player);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Media Player";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Player)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private AxWMPLib.AxWindowsMediaPlayer Player;
+        private System.Windows.Forms.ListBox list;
+        private System.Windows.Forms.Label fileName;
+        private System.Windows.Forms.Label Duration;
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
---
+++ Media Player C#/Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.IO;
+
+
+namespace Media_Player_C_
+{
+    public partial class Form1 : Form
+    {
+
+        List<string> filteredFiles = new List<string>();
+        FolderBrowserDialog browser = new FolderBrowserDialog();
+        int currentFile = 0;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LoadFolderEvent(object sender, EventArgs e)
+        {
+            Player.Ctlcontrols.stop();
+
+            if (filteredFiles.Count > 1) { 
+                filteredFiles.Clear();
+                filteredFiles = null;
+
+                list.Items.Clear();
+
+                currentFile = 0;
+            }
+
+            DialogResult result = browser.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                filteredFiles = Directory.GetFiles(browser.SelectedPath, "*.*").Where(file => file.ToLower().EndsWith("webm")
+                || file.ToLower().EndsWith("mp4") || file.ToLower().EndsWith("avi") || file.ToLower().EndsWith("wmv")
+                || file.ToLower().EndsWith("mkv") || file.ToLower().EndsWith("mp3")).ToList();
+
+                LoadPlaylist();
+            }
+
+        }
+
+        private void ShowAboutEvent(object sender, EventArgs e)
+        {
+            MessageBox.Show("This app is made by Miranda Marques at https://github.com/OMirandinha" + Environment.NewLine + "Hope you have fun with my media player ;)");
+
+        }
+
+        private void PlayerStateChangeEvent(object sender, AxWMPLib._WMPOCXEvents_PlayStateChangeEvent e)
+        {
+            if (e.newState == 0)
+            {
+                Duration.Text = "Media Player is ready to be loaded";
+            }
+            else if (e.newState == 1)
+            {
+                Duration.Text = "Duration: " + Player.currentMedia.durationString;
+            }
+            else if (e.newState == 8)  
+            {
+                
+                if (currentFile >= filteredFiles.Count - 1)
+                {
+                    currentFile = 0;  
+                }
+                else
+                {
+                    currentFile += 1;
+                }
+
+                list.SelectedIndex = currentFile;
+
+                
+                PlayFile(filteredFiles[currentFile]);  
+
+                ShowFileName(fileName);
+            }
+
+            else if (e.newState == 9)
+            {
+                Duration.Text = "Loading new video";
+            }
+
+            else if (e.newState == 10)
+            { 
+                timer.Start();
+            }
+        }
+
+        private void PlaylistChanged(object sender, EventArgs e)
+        {
+            currentFile = list.SelectedIndex;
+         
+            PlayFile(filteredFiles[currentFile]);  
+
+            ShowFileName(fileName);
+        }
+
+        private void TimerEvent(object sender, EventArgs e)
+        {
+            Player.Ctlcontrols.play();
+            timer.Stop();
+        }
+
+        private void LoadPlaylist()
+        {
+            Player.currentPlaylist = Player.newPlaylist("Playlist", "");
+
+            foreach (string videos in filteredFiles) 
+            {
+                Player.currentPlaylist.appendItem(Player.newMedia(videos));
+                list.Items.Add(Path.GetFileName(videos));
+
+
+
+            }
+
+            if (filteredFiles.Count > 0)
+            {
+                fileName.Text = "Files Found" + filteredFiles.Count;
+
+                list.SelectedIndex = currentFile;
+
+                
+                PlayFile(filteredFiles[currentFile]); 
+
+
+            }
+
+            else
+            {
+                MessageBox.Show("No Video Files Found in this folder");
+            }
+
+
+        }
+
+        private void PlayFile(string url)
+        {
+            Player.URL = url;
+        }
+
+        private void ShowFileName(Label name)
+        {
+            string file = Path.GetFileName(list.SelectedItem.ToString());
+            name.Text = "Currently Playing:" + file;
+        }
+
+      
+        
+
+
+
+    }
+}
